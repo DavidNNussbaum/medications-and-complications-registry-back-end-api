1b84be929c90e8b8b0d45ba8b935fe6ca42c32ca
DavidNNussbaum <brocknynj@aol.com>
2021-08-02 09:10:18 -0400

    Routes file  is configured.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :users
+  resources :complications, except: [:index]
+  resources :medications do 
+    resources :complications, only: [:index]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
 end
