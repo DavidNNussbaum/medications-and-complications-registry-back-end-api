88e618d76c847634649aa715169de535aaa49895
DavidNNussbaum <brocknynj@aol.com>
2021-08-10 20:36:00 -0400

    user_controller is modified.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
     skip_before_action :authorized
 
     def create
-        @user = user.find_by(email: user_params[:email])
+        @user = User.find_by(email: user_params[:email])
         if @user && @user.authenticate(user_params[:password])
             token = encode_token({user_id: @user.id})
             render json: { user: UserSerializer.new(@user).serializable_hash, token: token}, status: :created 
