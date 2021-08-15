6d1d0d1c7745471dc9631b5793849ebd2972e2dc
DavidNNussbaum <brocknynj@aol.com>
2021-08-15 11:53:31 -0400

    complication.rb is modified.

--- app/models/complication.rb
+++ app/models/complication.rb
@@ -1,6 +1,6 @@
 class Complication < ApplicationRecord
     belongs_to :medication
-    belongs_to :user, required: false
+    belongs_to :user 
     validates  :complication_severity, :complication_duration, :complication_description, :completely_resolved, presence: true
     validates :complication_severity, numericality: {less_than_or_equal_to: 10, greater_than_or_equal_to: 1}
     
